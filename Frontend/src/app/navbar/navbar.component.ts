aba152fbb311a15f5f8c8822a4dddfbd24b9360a
CHIRICO ANDREA <andrea.chirico@P-ACHIRICO.dgsgroup.it>
2020-03-24 10:35:40 +0100

    formFixed

--- Frontend/src/app/navbar/navbar.component.ts
+++ Frontend/src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -10,4 +11,5 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export class NavbarComponent {
   menuIcon = faBars;
+  accessForm: FormGroup;
 }
